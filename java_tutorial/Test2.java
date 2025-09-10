abfb42de7c22723b59e1eb85e3311046f768b030
Mar <mar.kiniec@gmail.com>
2025-09-10 08:59:04 -0500

    added class LowArray and TestLowArray to chapter 2

--- java_tutorial/Test2.java
+++ java_tutorial/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
         int x = 5;
         int [] y = new int [5];
         System.out.println(x);
-        System.out.println(y);
+        System.out.println(y); // prints out the address of reference data type array named y
     }
 
 
