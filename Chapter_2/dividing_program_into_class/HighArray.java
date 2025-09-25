5004cf524791bf4e7f3136c577e9c4cdeee5a404
Mar <mar.kiniec@gmail.com>
2025-09-25 16:00:10 -0500

    update

--- Chapter_2/dividing_program_into_class/HighArray.java
+++ Chapter_2/dividing_program_into_class/HighArray.java
@@ -1,22 +1,54 @@
 package Chapter_2.dividing_program_into_class;
 
-/*public class HighArray {
+public class HighArray {
     private long[] a; //data field array
     private int nElems; //data field number of elements
 
     public HighArray( int max ){
         a = new long[max];
     }//constructor
-     public boolean find( long searchKey ){
+     public boolean find( long searchKey ){ //looks through the array for the item whose value was passed to is as an argument / returns true or false
         int j;
         for(j=0 ; j<nElems ; j++){ //searching for
             if (a [j] ==searchKey){
                 break;
             }
         }//end of for
+      if(j == nElems){
+          return false;
+      }
+      else {
+          return true;
+      }
+    }//end find()
 
+    public void insert(long value){ //places a new data item in the next available space in the array/ nElems keeps track of the numbers of array cells that are actually filled with data items.
+        a[nElems]= value;
+        nElems++;
+    } // end of insert()
 
-     }//end find()
-
-
-}//end of class */
+    public boolean delete (long value) { //delete a value
+        int j;
+        for( j= 0; j < nElems; j++) { //look for value
+            if ( value == a[j]){ //found
+                break;
+            }//end if
+        }//end of for
+        if( j == nElems){
+            return false; //can't find
+        }//end if
+        else{
+            int k;
+            for( k = j; k<nElems; k++) {
+                a[k] = a[k + 1];
+            }//end of for
+            return true;
+        }//end else
+    }// end of boolean()
+    public void display(){ // display elements
+    for(int j =0; j <nElems; j++){
+        System.out.print(a[j] + " ");
+    }//end of for
+        System.out.println();
+    }//end of display()
+}//end of class HighArray
---
+++ Chapter_2/dividing_program_into_class/TestHighArray.java
@@ -0,0 +1,31 @@
+package Chapter_2.dividing_program_into_class;
+
+public class TestHighArray {
+    public static void main(String[]args){
+        int maxSize = 100;
+        HighArray arr = new HighArray(maxSize); //create object of HighArray
+        arr.insert(77); //insert 10 items first number being for index 0
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+        arr.display();
+        int searchkey = 35;
+        if( arr.find(searchkey)){
+            System.out.println("Found " + searchkey);
+        }//end if
+        else{
+            System.out.println("Can't find " + searchkey );
+        }//end else
+        arr.delete(00);
+        arr.delete(55);
+        arr.delete(99);
+        arr.display();
+
+    }// end of main()
+}// end of class TestHighArray
