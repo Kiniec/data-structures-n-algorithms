0436ef455a6983f45217604edab3fbb17700f373
Mar <mar.kiniec@gmail.com>
2025-09-25 15:33:27 -0500

    added to TestOrderedArray

--- Chapter_2/binary_search/OrderedArray.java
+++ Chapter_2/binary_search/OrderedArray.java
@@ -1,15 +1,18 @@
-/*package Chapter_2.binary_search;
+package Chapter_2.binary_search;
 
 public class OrderedArray {
-    private long[]a;
+    private int[]a;
     private int nElems;
-    public OrderedArray(int max) {
-        a = new long[max]; ///nElems= 0
+
+    public OrderedArray(int size) {
+        a = new int[size]; ///nElems= 0
     }///end of construtor
+
     public int size(){
         return nElems;
     }/// end of size
-    public int find(long searchKey) {
+
+    public int find(int searchKey) {
         int lowerBound = 0;
         int upperBound = nElems - 1;
         int curIn;
@@ -28,20 +31,49 @@ public class OrderedArray {
                     lowerBound = curIn + 1; /// it is in upper half
                 }///end of if
                 else {
-                    upperBound = curIn - 1; ///its in a lower half
+                    upperBound = curIn - 1; ///it's in a lower half
                 }///end of else
 
              }///end else
          }///end while
       }///end of find()
 
-public void insert ( long value ){
-          int j;
-          for (j = 0; j < nElems; j++){
-              if ( a[j] < nElems > value){
-                  break;
-              }///end if
-          }///end for
-}///end of insert
+    public void insert ( int value ){
+        if(nElems < a.length) {
+            int j;
+            for (j = 0; j < nElems; j++) {
+                if (a[j] > value) {
+                    break;
+                }///end if
+                }///end for
+                for( int k =nElems; k > j; k--) { ///moves bigger ones up
+                a[k] = a[k-1];
+                }///end for
+            a[j] = (int) value; /// insert it
+            nElems++;
+            }///end if
+            else{
+            System.out.println( "Cannot insert, array is full");
+            }///end else
+      }///end of insert
+
+    public boolean delete( int value){
+            int j = find(value);
+            if (j == nElems){
+                return false;///can't find it
+            }///end if
+            else{ ///found it
+                for(int k = j ; k < nElems -1; k++){ ///move bigger ones down,
+                ///use nElem-1 to make sure to not run over the length of the array
+                a[k] = a[k+1];
+                }///end for
+            nElems--; /// delete the number of elements by one.
+            return true;
+            }///end else
+        }///end delete
+    public void display(){
+        for(int j=0 ; j < nElems; j++){
+            System.out.print(a[j] + " ");
+        }
+    }///end of display
 } ///end class OrderArray
-*/
