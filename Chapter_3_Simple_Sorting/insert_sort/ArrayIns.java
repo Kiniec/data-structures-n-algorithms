c2768fd081d98b7abaf7c1326594beb1ad44f0a0
Mar <mar.kiniec@gmail.com>
2025-10-04 00:35:14 -0500

    added to chapter3 selectionsort

--- Chapter_3_Simple_Sorting/insert_sort/ArrayIns.java
+++ Chapter_3_Simple_Sorting/insert_sort/ArrayIns.java
@@ -1,4 +1,36 @@
 package Chapter_3_Simple_Sorting.insert_sort;
 
 public class ArrayIns {
-}
+    private long [] a;  //ref to array a
+    private int nElems; /// number of data items
+    public ArrayIns(int max){
+        a = new long[max]; /// create the array
+        nElems = 0;
+    }/// end of ArrayIns
+
+    public void insert(long value ){ ///put element into array
+        a[nElems] = value;             /// insert it
+        nElems++;                   ///increment size
+    }///end insert()
+
+    public void display(){
+        for( int j = 0; j < nElems; j++){      ///for each element
+            System.out.print(a[j]   + " ");  ///display it
+        }///end for
+        System.out.println(" ");
+    }///end display()
+
+    public void insertionSort(){
+        int in;
+        int out;
+        for(out =1 ; out < nElems ; out++){  /// out is dividing line
+            long temp = a[out];                 /// remove marked item
+            in = out;                           ///start shifts at out
+            while(in>0 && a[in-1] >= temp ){    ///until one is smaller
+                a[in] = a[in-1];                ///shift item to right
+                --in;                           ///go left one position
+            }///end while
+            a[in] = temp;                       ///insert marker item
+        }///end for
+    }///end insertionSort()
+}/// end class ArrayIns
--- Chapter_3_Simple_Sorting/insert_sort/InsertSortApp.java
+++ Chapter_3_Simple_Sorting/insert_sort/InsertSortApp.java
@@ -1,5 +1,24 @@
 package Chapter_3_Simple_Sorting.insert_sort;
 
 public class InsertSortApp {
+    public static void main(String[] args ){
+        int maxSize = 100;          /// array size
+        ArrayIns arr = new ArrayIns(maxSize); ///reference toa and create array
 
-}///
+        arr.insert (77);
+        arr.insert (99);
+        arr.insert (44);
+        arr.insert (55);
+        arr.insert (22);
+        arr.insert (88);
+        arr.insert (11);
+        arr.insert (00);
+        arr.insert (66);
+        arr.insert (33);
+
+        arr.display();
+        arr.insertionSort();
+        arr.display();
+    }///end main()
+
+}///end InsertSortApp
