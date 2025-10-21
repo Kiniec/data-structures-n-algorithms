79fcf9149931ab8e0e02477d730b321203256df1
Mar <mar.kiniec@gmail.com>
2025-10-20 20:21:58 -0500

    added chapter 5 Link List

---
+++ Chapter_5_Link_List/Link.java
@@ -0,0 +1,26 @@
+package Chapter_5_Link_List;
+
+public class Link {
+private int  x;
+public Link next;
+
+public Link(int number ) {/// constructor
+    x = number;
+}///end Link
+
+public void setX(){
+     x = number ;
+    }///emd setX()
+
+public int getX(){
+        return x;
+}///end get()
+
+ public void setNext(Link x) {
+    next = x;
+ }///end setNext
+    public Link getNext() {
+        return next;
+    }///end getNext
+
+}///end class Link
---
+++ Chapter_5_Link_List/LinkedList.java
@@ -0,0 +1,24 @@
+package Chapter_5_Link_List;
+
+public class LinkedList {
+
+
+    Private Link first;
+    public LinkedList() {
+         first = null;
+    }///end LinkedList
+    public boolean isEmpty() {
+    return first == null;
+    }///end isEmpty
+
+        public void insert(int number) {
+
+    Link newLink = new Link(number);
+}   ///end insert
+    public void setFirst(int number) {
+        Link l1 = new Link(number);
+        first = l1;
+    }///end setForst()
+
+
+}///end class LinkedList
